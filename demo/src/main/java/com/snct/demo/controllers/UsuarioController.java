f84529d77746bc408afb7a1dbad5e37af5525aeb
UABJ <isaac.adler@ufrpe.br>
2024-10-29 17:13:35 -0300

    UpdateMethod

--- demo/src/main/java/com/snct/demo/controllers/UsuarioController.java
+++ demo/src/main/java/com/snct/demo/controllers/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.snct.demo.domain.Conta;
 import com.snct.demo.domain.Usuario;
 import com.snct.demo.repositories.UsuarioRepository;
 
@@ -26,9 +27,17 @@ public class UsuarioController {
     }
 
     @PostMapping()
-    public ResponseEntity<?> postMethodName(@RequestBody Usuario entity) {
-        usuarioRepository.save(entity);
-        return ResponseEntity.ok(usuarioRepository.findAll());
+    public ResponseEntity<?> postMethodName(@RequestBody UsuarioDTO entity) {
+        Conta conta = Conta.builder().build();
+        
+        Usuario usuario = Usuario.builder()
+        .nome(entity.getNome())
+        .email(entity.getEmail())
+        .cpf(entity.getCPF())
+        .conta(conta)
+        .build();
+        usuarioRepository.save(usuario);
+        return ResponseEntity.ok().build();
     }
 
     
---
+++ demo/src/main/java/com/snct/demo/controllers/UsuarioDTO.java
@@ -0,0 +1,17 @@
+package com.snct.demo.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioDTO {
+
+    private String Nome;
+    private String Email;
+    private String CPF;
+}
